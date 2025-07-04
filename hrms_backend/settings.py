e8912dcddba90709f0f74f9dd2517e118b00c4ef
fenan-yosef <fenanyosef@gmail.com>
2025-07-04 18:31:53 +0300

    fix: Correct app name case in INSTALLED_APPS for consistency

--- hrms_backend/settings.py
+++ hrms_backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'HR',  # changed from 'hr'
+    'hr',  # changed from 'HR'
 ]
 
 MIDDLEWARE = [
