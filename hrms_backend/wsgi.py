f588f16c39903cb7e4cc73a914f6f86edfa77940
fenan-yosef <fenanyosef@gmail.com>
2025-07-04 18:50:52 +0300

    fix: Reorder import statements in wsgi.py for clarity

--- hrms_backend/wsgi.py
+++ hrms_backend/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import os.path
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
