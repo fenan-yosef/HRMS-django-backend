fb500a531229577bc3a73eef30e98f89bbf80283
fenan-yosef <fenanyosef@gmail.com>
2025-07-16 11:01:55 +0300

    feat: Enhance Employee model and serializer to handle password hashing and default values

--- employee/models.py
+++ employee/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from department.models import Department
 from django.conf import settings
+from django.contrib.auth.hashers import make_password
 
 class Employee(models.Model):
     first_name    = models.CharField(max_length=50)
@@ -11,8 +12,16 @@ class Employee(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     date_joined   = models.DateField(auto_now_add=True)
     is_active     = models.BooleanField(default=True)
-    password       = models.CharField(max_length=128, default='root')  # Store hashed password
+    password       = models.CharField(max_length=128)  # Store hashed password
     department    = models.ForeignKey(Department, on_delete=models.PROTECT, related_name='employees')
+
+    DEFAULT_PASSWORD = 'default_password'
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:  # If it's a new employee
+            if not self.password:
+                self.password = make_password(Employee.DEFAULT_PASSWORD)  # Hash the default password
+        super().save(*args, **kwargs)
--- employee/serializers.py
+++ employee/serializers.py
@@ -2,18 +2,41 @@ from rest_framework import serializers
 from .models import Employee
 from department.serializers import DepartmentSerializer
 from department.models import Department  # Import Department model
+from django.contrib.auth.hashers import make_password
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
     """Serializer for Employee model."""
     department = DepartmentSerializer(read_only=True)
+    password = serializers.CharField(write_only=True, required=False, default=None)
 
     class Meta:
         model = Employee
         fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True, 'required': False}
+        }
 
     def create(self, validated_data):
         department_id = self.context['request'].data.get('department')  # Get department ID from request data
         department = Department.objects.get(pk=department_id)  # Retrieve the Department instance
+        
+        password = validated_data.pop('password', None)
+        if password:
+            validated_data['password'] = make_password(password)
+        else:
+            validated_data['password'] = make_password(Employee.DEFAULT_PASSWORD)
+
         employee = Employee.objects.create(department=department, **validated_data)  # Create the Employee
         return employee
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.password = make_password(password)
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        
+        instance.save()
+        return instance
