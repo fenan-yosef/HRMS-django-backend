d208b637a0042c989d315aa2348aaa019587df63
fenan-yosef <fenanyosef@gmail.com>
2025-07-16 11:25:54 +0300

    feat: Update EmployeeSerializer to allow department updates via primary key and improve password handling

--- employee/serializers.py
+++ employee/serializers.py
@@ -7,8 +7,9 @@ from django.contrib.auth.hashers import make_password
 
 class EmployeeSerializer(serializers.ModelSerializer):
     """Serializer for Employee model."""
-    department = DepartmentSerializer(read_only=True)
-    password = serializers.CharField(write_only=True, required=False, default=None)
+    department = serializers.CharField(write_only=True, required=False)  # allow department update via pk
+    # Keep the read representation
+    department_details = DepartmentSerializer(source='department', read_only=True)
 
     class Meta:
         model = Employee
@@ -18,23 +19,30 @@ class EmployeeSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        department_id = self.context['request'].data.get('department')  # Get department ID from request data
-        department = Department.objects.get(pk=department_id)  # Retrieve the Department instance
-        
+        department_id = self.context['request'].data.get('department')
+        department = Department.objects.get(pk=department_id)
+
         password = validated_data.pop('password', None)
         if password:
             validated_data['password'] = make_password(password)
         else:
             validated_data['password'] = make_password(Employee.DEFAULT_PASSWORD)
 
-        employee = Employee.objects.create(department=department, **validated_data)  # Create the Employee
+        employee = Employee.objects.create(department=department, **validated_data)
         return employee
 
     def update(self, instance, validated_data):
+        # Pop the department field if provided
+        department_id = validated_data.pop('department', None)
+        if department_id is not None:
+            instance.department = Department.objects.get(pk=department_id)
+
+        # Update password if provided
         password = validated_data.pop('password', None)
         if password:
             instance.password = make_password(password)
-        
+
+        # Update all other fields
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         
